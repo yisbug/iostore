f6b6ec940adce946df0e5a1701b164c6ab72760c
yisbug <yisbug@qq.com>
2019-05-26 11:36:33 +0800

    chore: add eslint & prettier

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  parser: 'babel-eslint',
+  extends: ['airbnb', 'prettier', 'plugin:prettier/recommended'],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+};
