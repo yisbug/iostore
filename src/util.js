5a34f9abbdec63f37afe7505f098e0be8c5b7791
vienwu <vienwu@tencent.com>
2020-04-21 06:38:38 +0800

    fix: typeof object

--- src/util.js
+++ src/util.js
@@ -1,13 +1,12 @@
 export const isFunction = fn => typeof fn === 'function';
 export const isUndefined = prop => typeof prop === 'undefined';
-export const isObject = o => typeof o === 'object';
+export const isObject = o => Object.prototype.toString.call(o) === '[object Object]';
 export const isArray = o => Array.isArray(o);
 export const isPromise = fn => {
   if (fn instanceof Promise) return true;
   return isObject(fn) && isFunction(fn.then);
 };
 export const addProxy = (o, handler) => {
-  if (!isObject(o) || o === null) return o;
   if (isArray(o)) {
     o.forEach((item, index) => {
       if (isObject(item)) {
@@ -20,6 +19,8 @@ export const addProxy = (o, handler) => {
         o[key] = addProxy(o[key], handler);
       }
     });
+  } else {
+    return o;
   }
   return new Proxy(o, handler);
 };
